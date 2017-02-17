1355e243125047e80070b4f1945aa7b49a81c5ba
F1 race car team <ucichemecars@gmail.com>
2017-02-17 14:46:51 -0800

    Fixed to fit matlab
    Change the output style to fit that of matlab

--- Basic_Sodium_Thiosulfate/Basic_Sodium_Thiosulfate.ino
+++ Basic_Sodium_Thiosulfate/Basic_Sodium_Thiosulfate.ino
@@ -148,11 +148,12 @@ void advancedRead(void)
   uint16_t ir, full;
   ir = lum >> 16;
   full = lum & 0xFFFF;
-  Serial.print("[ "); Serial.print(millis()); Serial.print(" ms ] ");
-  Serial.print("IR: "); Serial.print(ir);  Serial.print("  ");
-  Serial.print("Full: "); Serial.print(full); Serial.print("  ");
-  Serial.print("Visible: "); Serial.print(full - ir); Serial.print("  ");
-  Serial.print("Lux: "); Serial.println(tsl.calculateLux(full, ir));
+  //Serial.print("[ "); Serial.print(millis()); Serial.print(" ms ] ");
+  //Serial.print("IR: "); Serial.print(ir);  Serial.print("  ");
+  //Serial.print("Full: "); Serial.print(full); Serial.print("  ");
+  //Serial.print("Visible: "); Serial.print(full - ir); Serial.print("  ");
+  //Serial.print("Lux: "); 
+  Serial.print(millis()); Serial.print("    "); Serial.println(tsl.calculateLux(full, ir));
 }
 
 /**************************************************************************/
