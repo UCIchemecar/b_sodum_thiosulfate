f357622f4b9f7924968e31e91f7278ca7b3bc85f
melonpocky <mayaz1@uci.edu>
2017-02-21 16:34:36 -0800

    Fixed it to remove random printings
    Fixed to remove the serial printing about the sensor information

--- Basic_Sodium_Thiosulfate/Basic_Sodium_Thiosulfate.ino
+++ Basic_Sodium_Thiosulfate/Basic_Sodium_Thiosulfate.ino
@@ -25,6 +25,7 @@ void displaySensorDetails(void)
 {
   sensor_t sensor;
   tsl.getSensor(&sensor);
+  /*
   Serial.println("------------------------------------");
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
   Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
@@ -34,6 +35,7 @@ void displaySensorDetails(void)
   Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" lux");  
   Serial.println("------------------------------------");
   Serial.println("");
+  */
 }
 
 /**************************************************************************/
@@ -58,30 +60,31 @@ void configureSensor(void)
   //tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
 
   /* Display the gain and integration time for reference sake */  
-  Serial.println("------------------------------------");
-  Serial.print  ("Gain:         ");
+  //Serial.println("------------------------------------");
+  //Serial.print  ("Gain:         ");
   tsl2591Gain_t gain = tsl.getGain();
   switch(gain)
   {
     case TSL2591_GAIN_LOW:
-      Serial.println("1x (Low)");
+      //Serial.println("1x (Low)");
       break;
     case TSL2591_GAIN_MED:
-      Serial.println("25x (Medium)");
+      //Serial.println("25x (Medium)");
       break;
     case TSL2591_GAIN_HIGH:
-      Serial.println("428x (High)");
+      //Serial.println("428x (High)");
       break;
     case TSL2591_GAIN_MAX:
-      Serial.println("9876x (Max)");
+      //Serial.println("9876x (Max)");
       break;
   }
+  /*
   Serial.print  ("Timing:       ");
   Serial.print((tsl.getTiming() + 1) * 100, DEC); 
   Serial.println(" ms");
   Serial.println("------------------------------------");
   Serial.println("");
-
+  */
   
 }
 
@@ -94,15 +97,15 @@ void setup(void)
 {
   Serial.begin(9600);
   
-  Serial.println("Starting Adafruit TSL2591 Test!");
+  //Serial.println("Starting Adafruit TSL2591 Test!");
   
   if (tsl.begin()) 
   {
-    Serial.println("Found a TSL2591 sensor");
+    //Serial.println("Found a TSL2591 sensor");
   } 
   else 
   {
-    Serial.println("No sensor found ... check your wiring?");
+    //Serial.println("No sensor found ... check your wiring?");
     while (1);
   }
     
